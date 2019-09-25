dad0424b24ede0ccb845ba2808306a7b96d5ccf4
Matt Groberg <grobergmg@gmail.com>
2019-09-25 14:06:12 -0700

    add dollar float animation

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+const navbar=document.querySelector('nav');
+
 const animatedElements=document.querySelectorAll('.animated')
 
-const checkInterval=window.setInterval(checkPositions,500)
+window.addEventListener('scroll',checkPositions)
 
 function checkPositions(){
-	console.log('checking')
+	console.log(navbar,window.scrollY)
+	window.scrollY===0 ? navbar.classList.remove('navChange') : navbar.classList.add('navChange')
 	animatedElements.forEach((item,i)=>{
 		const position=item.getBoundingClientRect()
 		if(position.top+position.height/2<=window.innerHeight){
 			item.style['animation-play-state']="running";
-			if(i+1===animatedElements.length){
-				window.clearInterval(checkInterval)
-			}
 		}
 	})
 }
