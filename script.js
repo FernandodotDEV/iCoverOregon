4a71d5f4c1e42cf08bdf9384117901cce1636ee4
Matt Groberg <grobergmg@gmail.com>
2019-09-26 12:18:28 -0700

    scale for mobile

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
+window.addEventListener('scroll',checkPositions)
+
 const navbar=document.querySelector('nav');
 
 const animatedElements=document.querySelectorAll('.animated')
 
-window.addEventListener('scroll',checkPositions)
 
 function checkPositions(){
-	console.log(navbar,window.scrollY)
+	console.log('scrolling')
 	window.scrollY===0 ? navbar.classList.remove('navChange') : navbar.classList.add('navChange')
 	animatedElements.forEach((item,i)=>{
 		const position=item.getBoundingClientRect()
 		if(position.top+position.height/2<=window.innerHeight){
-			item.style['animation-play-state']="running";
+			item.classList.add('onScreen')
 		}
 	})
+}
+
+function smoothScroll(location){
+	document.querySelector(location).scrollIntoView({behavior:'smooth'})
 }
 
 
